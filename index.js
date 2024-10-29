019cb52472796bb022c8fe4605e425e496e462f2
Nili Scharf <nilischarf@gmail.com>
2024-10-29 13:15:44 +0200

    complete control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  let greeting;
+  if (number <= 400){
+    greeting='This one is on me!'
+  }
+    else if (number<2000){
+      greeting='That will be twenty bucks.'
+    }
+    else if (number>2000){
+      greeting='I will gladly take your thirty bucks.'
+      if (number>2500){
+        greeting='No can do.'
+      }
+    }
+  return greeting
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let ternary;
+  city === "NYC" ? (ternary='Ok, sounds good.') : (ternary='No go.')
+  return ternary
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let statement;
+  switch(tip) {
+    case 'generous':
+      statement='Thank you so much.';
+      break;
+    case 'not as generous':
+      statement='Thank you.';
+      break;
+    default:
+      statement='Bye.';
+      break;
+  }
+  return statement
 }
